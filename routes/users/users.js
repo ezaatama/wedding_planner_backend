6255843be87c0dc43ea4b483a2243c9e3452d2e0
ezaatama <ezatama33@gmail.com>
2024-06-10 13:41:59 +0700

    update routes users

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { createUser, findAllUser } = require("../../controllers/users/users");
+const { createUser, findAllUser, findUserById, updateUser, deleteUser } = require("../../controllers/users/users");
 const middlewareToken = require("../../middleware/jwtToken");
 const router = express.Router();
 
 router.post('/users', createUser);
 router.get('/users', middlewareToken, findAllUser);
+router.get('/users/:uuid', middlewareToken, findUserById);
+router.patch('/users/:uuid', middlewareToken, updateUser);
+router.delete('/users/:uuid', middlewareToken, deleteUser);
 
 module.exports = router;
