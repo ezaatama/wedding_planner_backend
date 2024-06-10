7ed87a69d8179da4899d896b92fd5a7b726d6fcb
ezaatama <ezatama33@gmail.com>
2024-06-10 13:42:06 +0700

    update routes auth

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { login } = require("../../controllers/auth/auth");
-// const middlewareToken = require("../../middleware/jwtToken");
+const { login, me, changePass, logout } = require("../../controllers/auth/auth");
+const middlewareToken = require("../../middleware/jwtToken");
 const router = express.Router();
 
 router.post('/login', login);
+router.get('/me', middlewareToken, me);
+router.post('/change-pass', middlewareToken, changePass);
+router.post('/logout', middlewareToken, logout);
 
 module.exports = router;
