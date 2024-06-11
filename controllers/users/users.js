0a2f9f4e663fb0770b3e8a166b853531c04a14bd
ezaatama <ezatama33@gmail.com>
2024-06-11 10:01:05 +0700

    update controller users

--- controllers/users/users.js
+++ controllers/users/users.js
@@ -1,4 +1,5 @@
 const Users = require("../../models/users");
+const { Op } = require("sequelize");
 const bcrypt = require("bcrypt");
 
 const createUser = async (req, res) => {
@@ -11,6 +12,17 @@ const createUser = async (req, res) => {
     const hashPassword = await bcrypt.hash(password, salt);
 
     try {
+        const existingUser = await Users.findOne({
+            where: {
+                [Op.or]: [{ username: username }, { email: email }]
+            },
+            paranoid: false // Sertakan entri yang dihapus dalam pencarian
+        });
+
+        if (existingUser && !existingUser.deletedAt) {
+            return res.status(400).responseNoData(400, false, "Username atau email sudah terdaftar");
+        }
+
         await Users.create({
             username: username,
             email: email,
@@ -30,39 +42,45 @@ const findAllUser = async (req, res) => {
     const limit = parseInt(req.query.limit) || 10;
     const offset = (page - 1) * limit;
     try {
-        const result = await Users.findAndCountAll({
-            attributes: ["uuid", "username", "email", "full_name", "phone"],
-            limit,
-            offset
-        });
-
-        const totalItems = result.count;
-        const totalPages = Math.ceil(totalItems / limit);
-
-        if (page > totalPages) {
-            // Jika halaman yang diminta melebihi total halaman yang ada, kirim respons 204 (No Content).
-            return res.status(204).end();
-          }
-
-          const response = {
-            code: 200,
-            status: true,
-            message: "Data users berhasil diambil!",
-            data: result.rows,
-            meta: {
-                totalItems,
-                totalPages,
-                currentPage: page
-            }
-          }
-
-          if (page > 1) {
-            response.meta.prevPage = page - 1;
-          }
-          if (page < totalPages) {
-            response.meta.nextPage = page + 1;
-          }
-          res.status(200).json(response);
+        const accountId = req.role;
+        
+        if (accountId === "admin") {
+            const result = await Users.findAndCountAll({
+                attributes: ["uuid", "username", "email", "full_name", "phone"],
+                limit,
+                offset
+            });
+    
+            const totalItems = result.count;
+            const totalPages = Math.ceil(totalItems / limit);
+    
+            if (page > totalPages) {
+                // Jika halaman yang diminta melebihi total halaman yang ada, kirim respons 204 (No Content).
+                return res.status(204).end();
+              }
+    
+              const response = {
+                code: 200,
+                status: true,
+                message: "Data users berhasil diambil!",
+                data: result.rows,
+                meta: {
+                    totalItems,
+                    totalPages,
+                    currentPage: page
+                }
+              }
+    
+              if (page > 1) {
+                response.meta.prevPage = page - 1;
+              }
+              if (page < totalPages) {
+                response.meta.nextPage = page + 1;
+              }
+              res.status(200).json(response);
+        } else {
+            res.status(403).responseNoData(403, false, "Anda tidak memiliki izin akses halaman ini!");
+        }
     } catch (error) {
         res.status(500).responseNoData(500, false, error.message);
     }
@@ -78,7 +96,7 @@ const findUserById = async (req, res) => {
         });
         if (!response) return res.status(404).responseNoData(404, false, "Data detail akun tidak ditemukan!");
 
-        return res.status(200).responseWithData(200, true, "Data detail akun berhasil diambil!", response);
+        res.status(200).responseWithData(200, true, "Data detail akun berhasil diambil!", response);
     } catch (error) {
         res.status(500).responseNoData(500, false, error.message);
     }
@@ -116,12 +134,12 @@ const updateUser = async (req, res) => {
                     uuid: req.params.uuid
                 }
             });
-            return res.status(201).responseNoData(201, true, "Akun berhasil diubah!");
+             res.status(201).responseNoData(201, true, "Akun berhasil diubah!");
         } else {
-            return res.status(403).responseNoData(403, false, "Tidak diizinkan mengubah akun!");
+             res.status(403).responseNoData(403, false, "Tidak diizinkan mengubah akun!");
         }
     } catch (error) {
-        return res.status(500).responseNoData(500, false, error.message);
+         res.status(500).responseNoData(500, false, error.message);
     }
 }
 
@@ -143,12 +161,12 @@ const deleteUser = async (req, res) => {
                   uuid: result.uuid,
                 },
               });
-            return res.status(200).responseNoData(200, true, "Akun berhasil dihapus!");
+             res.status(200).responseNoData(200, true, "Akun berhasil dihapus!");
         } else {
-            return res.status(403).responseNoData(403, false, "Tidak diizinkan menghapus data users!");
+             res.status(403).responseNoData(403, false, "Tidak diizinkan menghapus data users!");
         }
     } catch (error) {
-        return res.status(500).responseNoData(500, false, error.message);
+         res.status(500).responseNoData(500, false, error.message);
     }
 }
 
