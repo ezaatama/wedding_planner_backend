1bfda80004289817a0237a2dc639e63f0682ea07
ezaatama <ezatama33@gmail.com>
2024-06-11 10:00:50 +0700

    update jwt verify request

--- middleware/jwtToken.js
+++ middleware/jwtToken.js
@@ -6,9 +6,9 @@ const verifyToken = (req, res, next) => {
     if (token == null) return res.status(401).json({ message: "Unauthorized"});
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) return res.status(401).responseNoData(401, false, "Sesi Anda Telah Habis!");
-        req.email = decoded.email;
         req.user = decoded.uuid;
-        req.role = decoded.role;
+        req.role = decoded.roleId;
+        req.email = decoded.emailId;
 
         next();
     });
