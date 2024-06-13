014a0101d4672f47fc5218c44523cdbf46e62ae3
ezaatama <ezatama33@gmail.com>
2024-06-13 11:03:34 +0700

    update routes wedding

--- routes/wedding/wedding.js
+++ routes/wedding/wedding.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { createWeddings, findWedding, findWeddingById } = require("../../controllers/weddings/wedding");
+const { createWeddings, findWedding, findWeddingById, updateWedding, deleteWedding } = require("../../controllers/weddings/wedding");
 const middlewareToken = require("../../middleware/jwtToken");
 const router = express.Router();
 
 router.post('/weddings', middlewareToken, createWeddings);
 router.get('/weddings', middlewareToken, findWedding);
-router.get('/weddings/:id', middlewareToken, findWeddingById);
+router.get('/weddings/:uuid', middlewareToken, findWeddingById);
+router.patch('/weddings/:uuid', middlewareToken, updateWedding);
+router.delete('/weddings/:uuid', middlewareToken, deleteWedding);
 
 module.exports = router;
