3e09d7e91db5c4553c3fb5c075eecd8ce31c6efc
ezaatama <ezatama33@gmail.com>
2024-07-01 13:34:40 +0700

    update migration

--- config/migration.js
+++ config/migration.js
@@ -8,111 +8,20 @@ const Images = require("../models/images");
 const AdminActions = require("../models/admin_actions");
 const Designs = require("../models/designs");
 
-//ASOSIASI USERS
-Users.hasOne(Weddings, {
-  foreignKey: 'user_id',
-  sourceKey: 'uuid',
-  as: 'wedding'
-});
 
-Weddings.belongsTo(Users, {
-  foreignKey: 'user_id',
-  targetKey: 'uuid',
-  as: 'user'
-});
 
-//ASOSIASI WEDDINGS
-Weddings.hasMany(Guests, {
-  foreignKey: 'wedding_id',
-  sourceKey: 'uuid',
-  as: 'guests'
-});
 
-Guests.belongsTo(Weddings, {
-  foreignKey: 'wedding_id',
-  targetKey: 'uuid',
-  as: 'wedding'
-});
 
-//ASOSIASI RESERVATION
-Guests.hasOne(Reservations, {
-  foreignKey: 'guest_id',
-  sourceKey: 'id',
-  as: 'reservations'
-});
 
-Reservations.belongsTo(Weddings, {
-  foreignKey: 'wedding_id',
-  targetKey: 'uuid',
-  as: 'wedding'
-});
 
-Reservations.belongsTo(Guests, {
-  foreignKey: 'guest_id',
-  targetKey: 'id',
-  as: 'guest'
-});
 
-//ASOSIASI MESSAGES
-Guests.hasOne(Messages, {
-  foreignKey: 'guest_id',
-  sourceKey: 'id',
-  as: 'messages'
-});
 
-Messages.belongsTo(Weddings, {
-  foreignKey: 'wedding_id',
-  targetKey: 'uuid',
-  as: 'wedding'
-});
 
-Messages.belongsTo(Guests, {
-  foreignKey: 'guest_id',
-  targetKey: 'id',
-  as: 'guest'
-});
 
-//ASOSIASI IMAGES
-Weddings.hasMany(Images, {
-  foreignKey: 'wedding_id',
-  sourceKey: 'uuid',
-  as: 'images'
-});
 
-Images.belongsTo(Weddings, {
-  foreignKey: 'wedding_id',
-  targetKey: 'uuid',
-  as: 'wedding'
-});
-
-//ASOSIASI ADMIN ACTIONS
-Users.hasMany(AdminActions, {
-  foreignKey: 'admin_id',
-  sourceKey: 'uuid',
-  as: 'admin_action'
-});
-
-AdminActions.belongsTo(Users, {
-  foreignKey: 'admin_id',
-  targetKey: 'uuid',
-  as: 'user'
-});
-
-//ASOSIASI DESIGNS
-Weddings.hasOne(Designs, {
-  foreignKey: 'wedding_id',
-  sourceKey: 'uuid',
-  as: 'design'
-});
-
-Designs.belongsTo(Weddings, {
-  foreignKey: 'wedding_id',
-  targetKey: 'uuid',
-  as: 'wedding'
-});
 
 sequelize
-  .sync({force: true})
+  .sync()
   .then(() => {
     console.log("Database connected...");
     // Users.sync({alter: true});
@@ -123,4 +32,4 @@ sequelize
     console.error(err);
   });
 
-module.exports = { Users, Weddings, Guests, Reservations, Images, AdminActions, Designs };
+module.exports = { Users, Weddings, Guests, Reservations, Images, AdminActions, Designs, Messages };
