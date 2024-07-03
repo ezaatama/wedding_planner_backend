2aeb9328afa8d5a601c11898fb8365c144d9fb4f
ezaatama <ezatama33@gmail.com>
2024-07-03 08:37:31 +0700

    update guests routes

--- routes/guests/guests.js
+++ routes/guests/guests.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { createGuests, findGuests, findGuestById, updateGuest, deleteGuests } = require("../../controllers/guests/guests");
 const middlewareToken = require("../../middleware/jwtToken");
+const replaceString = require("../../middleware/utils");
 const router = express.Router();
 
 router.post('/guests', middlewareToken, createGuests);
 router.get('/guests', middlewareToken, findGuests);
-router.get('/guest', middlewareToken, findGuestById);
+router.get('/guest/:groomBride', middlewareToken, replaceString, findGuestById);
 router.patch('/guests/:id', middlewareToken, updateGuest);
 router.delete('/guests/:id', middlewareToken, deleteGuests);
 
