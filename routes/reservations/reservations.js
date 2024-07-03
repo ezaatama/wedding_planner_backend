57b5eb3ed6e401b1352f7666a27de4af816a4790
ezaatama <ezatama33@gmail.com>
2024-07-03 08:37:09 +0700

    update reservation routes

--- routes/reservations/reservations.js
+++ routes/reservations/reservations.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const { findReservation, updateReservation } = require("../../controllers/reservations/reservations");
+const { updateReservation } = require("../../controllers/reservations/reservations");
 const middlewareToken = require("../../middleware/jwtToken");
 const router = express.Router();
 
-router.get('/reservation', middlewareToken, findReservation);
 router.put('/update-reservation', middlewareToken, updateReservation);
 
 module.exports = router;
