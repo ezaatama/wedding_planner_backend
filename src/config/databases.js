16112677ddb9cf1e18069bc5f017c4c1a3f4da11
ezaatama <ezatama33@gmail.com>
2024-07-04 10:28:53 +0700

    update databases

--- src/config/databases.js
+++ src/config/databases.js
@@ -3,7 +3,8 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
     "wedding_planner", "root", "", {
         host: "localhost",
-        dialect: "mysql"
+        dialect: "mysql",
+        dialectModule: mysql2,
     }
 )
 
